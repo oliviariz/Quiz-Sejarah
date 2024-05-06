194a5d22128bb2019d85edb27a14859f1aecce55
oliviariz <132645290+oliviariz@users.noreply.github.com>
2024-05-06 10:55:55 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,93 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Jatuhnya kota Konstantinopel ke tangan Turki Usmani membawa dampak",
+    answer: "Penjelajahan samudra oleh bangsa barat",
+    options: ["Kemajuan Kota Konstantinope", "Penjelajahan samudra oleh bangsa barat", "Pecahnya perang salib/perang suci"],
+  },
+  {
+    numb: 2,
+    question: "Pada tahun 1511, Portugis berhasil menaklukkan Malaka. Meskipun demikian, Portugis tidak bisa sepenuhnya menguasai perdagangan di Asia karena beberapa hal berikut, kecuali",
+    answer: "Serangan dan perlawanan balik dari Kesultanan Malaka",
+    options: ["Serangan dan perlawanan balik dari Kesultanan Malaka", "Pedagang-pedagang Asia pindah ke pelabuhan lain yang aman", "Tindakan korupsi yang dilakukan oleh pejabat Portugis di Malaka"],
+  },
+  {
+    numb: 3,
+    question: "Kepulauan Banda merupakan salah satu penghasil pala terbaik dunia. Pada tahun 1621, VOC di bawah J P Coen membantai penduduk Banda. Salah satu dampak dari peristiwa tersebut adalah",
+    answer: "Berkurangnya petani yang memahami tentang budidaya pala",
+    options: ["VOC berhasil memonopoli komoditas pala di dunia", "Timbulnya berbagai perlawanan balas dendam rakyat Belanda", "Berkurangnya petani yang memahami tentang budidaya pala"],
+  },
+  {
+    numb: 4,
+    question: "Pada awal abad ke-19 terjadi perlawanan rakyat Maluku terhadap Belanda. Perlawanan yang dipimpin oleh Pattimura ini dilatarbelakangi oleh",
+    answer: "Penerapan monopoli cengkeh dan kerja rodi oleh Belanda",
+    options: ["Penerapan monopoli cengkeh dan kerja rodi oleh Belanda", "Praktik pelayaran hongi yang memusnahkan tanaman pala", "Perebutan lahan perkebunan pala dengan Belanda"],
+  },
+  {
+    numb: 5,
+    question: "Hak VOC untuk menebang tanaman rempah-rempah di Maluku adalah",
+    answer: "Ekstirpasi",
+    options: ["Poonalie sanctie", "Hongi", "Ekstirpasi"],
+  },
+  {
+    numb: 6,
+    question: "Makin luasnya wilayah VOC berakibat kemunduran VOC itu sendiri, sebab",
+    answer: "Makin banyaknya korupsi",
+    options: ["Bersaing dengan Portugis", "Makin banyaknya korupsi", "Biaya perang yang terlalu besar"],
+  },
+  {
+    numb: 7,
+    question: "Politik etis diusulkan oleh",
+    answer: "Vandeventer",
+    options: ["Vandeventer", "Douwes Dekker", "Van der Putte"],
+  },
+  {
+    numb: 8,
+    question: "Salah satu dampak negatif dari kolonialisme Belanda adalah munculnya sentimen rasial. Hal ini disebabkan oleh",
+    answer: "Belanda menerapkan berbagai aturan yang diskriminatif",
+    options: ["Belanda menerapkan berbagai aturan yang diskriminatif", "Belanda hanya mengakui kehebatan orang Eropa", "Penduduk lokal iri dengan kekayaan bangsa Eropa"],
+  },
+  {
+    numb: 9,
+    question: "Perjanjian Bongaya adalah perjanjian yang mengakhiri konflik antara VOC dan kesultanan",
+    answer: "Makasar",
+    options: ["Makasar", "Mataram", "Demak"],
+  },
+  {
+    numb: 10,
+    question: "Bangsa Barat yang bukan menjajah di Indonesia adalah",
+    answer: "Perancis",
+    options: ["Belanda", "Perancis", "Inggris"],
+  },
+  {
+    numb: 11,
+    question: "Bangsa Penjelajah saling bersaing dan ingin berkuasa di dunia baru yang ditemukannya, hal tersebut merupakan tujuan Bangsa Eropa melakukan penjelajahan yang disebut",
+    answer: "Glory",
+    options: ["Gold", "Gospel", "Glory"],
+  },
+  {
+    numb: 12,
+    question: "Bangsa Spanyol yang dianggap sebagai pelopor dalam pelayaran dan penjelajahan samudera adalah",
+    answer: "Christopher Colombus",
+    options: ["Christopher Colombus", "Sebastian Del Cano", "Magelhaens"],
+  },
+  {
+    numb: 13,
+    question: "Magelhaens merupakan tokoh penjelajah asal Spanyol yang dibunuh oleh suku Mactan yang terdapat di negara",
+    answer: "Filipina",
+    options: ["Malaysia", "Filipina", "Guam"],
+  },
+  {
+    numb: 14,
+    question: "Komoditi nusantara yang memicu terjadinya penjelajahan samudera adalah",
+    answer: "Rempah rempah",
+    options: ["Beras", "Fauna", "Rempah rempah"],
+  },
+  {
+    numb: 15,
+    question: "Dampak positif tanam paksa bagi penduduk Jawa adalah",
+    answer: "Rakyat mengenal jenis tanaman perdagangan",
+    options: ["Lahan pertanian semakin luas", "Rakyat mengenal jenis tanaman perdagangan", "Kas pemerintahan Belanda semakin penuh"],
+  },
+];
